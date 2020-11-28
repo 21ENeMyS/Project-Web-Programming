9b6ab68ef1f26c9336227d63b0c6d2e3bddb17fb
muhamad farhan <delltop231@gmail.com>
2020-11-28 14:30:31 +0700

    Menambahkan Query Builder dalam Pupmart_Model di Folder Core

--- application/core/Pupmart_Model.php
+++ application/core/Pupmart_Model.php
@@ -8,6 +8,7 @@
  * @package ${NAMESPACE}
  */
 
+use function PHPSTORM_META\type;
 
 defined('BASEPATH') or exit('No direct script access allowed');
 
@@ -25,6 +26,96 @@ class Pupmart_Model extends CI_Model
         str_replace('_model', '', get_class($this))
       );
     }
+  }
+
+
+  public function validate()
+  {
+    /**
+     * Fungsi Validasi Input
+     * Peraturan Dideklarasikan dalam masing-masing model
+     * @author Almarup21 <https://github.com/almarup21>
+     * @package ${NAMESPACE}
+     */
+
+    $this->load->library('form_validation');
+
+    $this->form_validation->set_error_delimeters(
+      '<small class="form-text text-danger">',
+      '</small>'
+    );
+
+    $validationRules = $this->getValidationRules();
+    $this->form_validation->set_rules($validationRules);
+    return $this->form_validation->run();
+  }
+
+  public function select($column)
+  {
+    $this->db->select($column);
+    return $this;
+  }
+
+  public function where($column, $condition)
+  {
+    $this->db->where($column, $condition);
+    return $this;
+  }
+
+  public function like($column, $condition)
+  {
+    $this->db->like($column, $condition);
+    return $this;
+  }
+
+  public function orLike($column, $condition)
+  {
+    $this->db->or_like($column, $condition);
+    return $this;
+  }
+
+  public function join($table, $type = 'left')
+  {
+    $this->db->join($table, "$this->table.id_$table = $table.id", $type);
+    return $this;
+  }
+
+  public function orderBy($column, $order = 'asc')
+  {
+    $this->db->order_by($column, $order);
+    return $this;
+  }
+
+  public function first()
+  {
+    return $this->db->get($this->table)->row();
+  }
+
+  public function getAll()
+  {
+    return $this->db->get($this->table)->result();
+  }
+
+  public function count()
+  {
+    return $this->db->count_all_results($this->table);
+  }
+
+  public function create($data)
+  {
+    $this->db->insert($this->table, $data);
+    return $this->db->insert_id();
+  }
+
+  public function update($data)
+  {
+    return $this->db->update($this->table, $data);
+  }
+
+  public function delete()
+  {
+    $this->db->delete($this->table);
+    return $this->db->affected_rows();
   }
 }
 
