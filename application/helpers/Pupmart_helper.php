dcb9fbe2b783c6a18eba1d5ffe8000a776879b7d
muhamad farhan <delltop231@gmail.com>
2020-11-28 16:48:49 +0700

    Membuat File didalam Folder helper membuat code untuk password dan verify password,membuat code categories Dan menkonfigurasi Timezone didalam File index.php

---
+++ application/helpers/Pupmart_helper.php
@@ -0,0 +1,72 @@
+<?php
+
+function getDropdownList($table, $column)
+{
+  $CI = &get_instance();
+  $query = $CI->db->select($column)->form($table)->get();
+
+  if ($query->num_rows() >= 1) {
+    $option1 = ['' =>  '_ select _'];
+    $option2 = array_column($query->result_array(), $column[1], $column[0]);
+    $options = $option1 + $option2;
+
+    return $options;
+  }
+
+  return $options = [' ' => '_ select _'];
+}
+
+function getCategories()
+{
+  $CI = &get_instance();
+  $query = $CI->db->get('category')->result();
+  return $query;
+}
+
+/**
+ * Fungsi Getcard untuk menghitung jumlah data belanja Pembeli atau user 
+ * 
+ * 
+ * @author almarup21 <https://github.com/almarup21>
+ * @package ${NAMESPACE}
+ */
+
+function getCart()
+{
+  $CI = &get_instance();
+  $userId = $CI->session->userdata('id');
+  if ($userId) {
+    $query = $CI->db->where('id_user', $userId)->count_all_results('cart');
+    return $query;
+  }
+}
+
+/**
+ * Membuat password hashing atau meng-amankan password
+ * 
+ * @author almarup21 <https://github.com/almarup21>
+ * @package ${NAMESPACE}
+ */
+
+function hashEncrypt($input)
+{
+  $hash = password_hash($input, PASSWORD_DEFAULT);
+  return $hash;
+}
+
+/**
+ * Verifikasi Password jika password sama akan mengambalikan nilai true
+ * Dan jika salah akan mengembalikan nilai false atau gagal
+ * 
+ * @author almarup21 <https:///github.com/almarup21>
+ * @package ${NAMESPACE}
+ */
+
+function hashEncryptVerify($input, $hash)
+{
+  if (password_verify($input, $hash)) {
+    return true;
+  } else {
+    return false;
+  }
+}
